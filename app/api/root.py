0d92edbb68f172e35a84559097720f19a6661288
thinh@boot.ai <thinh@boot.ai>
2017-07-26 23:46:14 +0700

    add angular started

--- app/api/root.py
+++ app/api/root.py
@@ -2,11 +2,10 @@ from flask import request, Blueprint, redirect
 from flask import jsonify
 from app.extensions import client
 
-root = Blueprint('api', __name__)
+root = Blueprint('api', __name__, static_url_path="", static_folder="../fosec/static",
+                 template_folder="../fosec/static")
 
 
 @root.route('/', methods=['GET'])
 def index():
     return jsonify({'result': 'Success!!'})
-
-
