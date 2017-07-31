149375e6f7416a9300d6c1447bd5db6d76cb8a59
thinh@boot.ai <thinh@boot.ai>
2017-07-31 20:13:52 +0700

    edit app bootstrap

--- app/api/root.py
+++ app/api/root.py
@@ -1,12 +1,12 @@
 from flask import request, Blueprint, redirect, render_template, send_file, jsonify
 from app.extensions import client
 
-root = Blueprint('root', __name__)
+root = Blueprint('', __name__)
 
 
 @root.route('/', methods=['GET'])
 def index():
-    return send_file('index.html')
+    return send_file('./client/dist/index.html')
 
 
 
